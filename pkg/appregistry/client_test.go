tkashem/operator-marketplace-old

Reconcile operatorsource type by pullng down operator manifest from specified app registry server and making it available to the cluster

pkg/appregistry/client_test.go
@@ -1,0 +1,61 @@
+package appregistry
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/golang/mock/gomock"
+	appr_models "github.com/operator-framework/go-appr/models"
+)
+
+func TestRetrieveOneHappyPath(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	adapter := NewMockapprApiAdapter(controller)
+	decoder := NewMockblobDecoder(controller)
+	unmarshaller := NewMockblobUnmarshaller(controller)
+
+	client := client{
+		adapter:      adapter,
+		decoder:      decoder,
+		unmarshaller: unmarshaller,
+	}
+
+	namespace := "redhat"
+	repository := "foo"
+	release := "1.0"
+	digest := "abcdefgh"
+
+	pkg := &appr_models.Package{Content: &appr_models.OciDescriptor{
+		Digest: digest,
+	}}
+	adapter.EXPECT().GetPackageMetadata(namespace, repository, release).Return(pkg, nil).Times(1)
+
+	blobExpected := []byte{'e', 'n', 'c', 'o', 'd', 'e', 'd'}
+	adapter.EXPECT().DownloadOperatorManifest(namespace, repository, digest).Return(blobExpected, nil).Times(1)
+
+	decodedExpected := []byte{'d', 'e', 'c', 'o', 'd', 'e', 'd'}
+	decoder.EXPECT().Decode(blobExpected).Return(decodedExpected, nil).Times(1)
+
+	manifestExpected := &Manifest{
+		Publisher: "redhat",
+		Data: Data{
+			CRDs:     "my crds",
+			CSVs:     "my csvs",
+			Packages: "my packages",
+		},
+	}
+	unmarshaller.EXPECT().Unmarshal(decodedExpected).Return(manifestExpected, nil)
+
+	metadata, err := client.RetrieveOne(fmt.Sprintf("%s/%s", namespace, repository), release)
+
+	assert.NoError(t, err)
+	assert.Equal(t, namespace, metadata.Namespace)
+	assert.Equal(t, repository, metadata.Repository)
+	assert.Equal(t, release, metadata.Release)
+	assert.Equal(t, digest, metadata.Digest)
+	assert.Equal(t, manifestExpected, metadata.Manifest)
+}

pkg/operatorsource/handler.go
@@ -1,0 +1,49 @@
+package operatorsource
+
+import (
+	"context"
+
+	"github.com/operator-framework/operator-marketplace/pkg/apis/marketplace/v1alpha1"
+	"github.com/operator-framework/operator-sdk/pkg/sdk"
+	"github.com/sirupsen/logrus"
+)
+
+type Handler interface {
+	// Handle hanldes a new event generated by the operator sdk
+	Handle(ctx context.Context, event sdk.Event) error
+}
+
+type handler struct {
+	reconciler Reconciler
+}
+
+func (h *handler) Handle(ctx context.Context, event sdk.Event) error {
+	os := event.Object.(*v1alpha1.OperatorSource)
+
+	if event.Deleted {
+		logrus.Infof("No action taken, object has been deleted [type=%s object=%s/%s]",
+			os.TypeMeta.Kind, os.ObjectMeta.Namespace, os.ObjectMeta.Name)
+
+		return nil
+	}
+
+	reconciled, err := h.reconciler.IsAlreadyReconciled(os)
+	if err != nil {
+		return err
+	}
+
+	if reconciled {
+		logrus.Infof("Already reconciled, no action taken [type=%s object=%s/%s]",
+			os.TypeMeta.Kind, os.ObjectMeta.Namespace, os.ObjectMeta.Name)
+
+		return nil
+	}
+
+	if err := h.reconciler.Reconcile(os); err != nil {
+		return err
+	}
+
+	logrus.Infof("Reconciliation completed successfully [type=%s object=%s/%s]",
+		os.TypeMeta.Kind, os.ObjectMeta.Namespace, os.ObjectMeta.Name)
+	return nil
+}

operator-framework/operator-marketplace

pkg/appregistry/client_test.go
@@ -1,0 +1,61 @@
+package appregistry
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/golang/mock/gomock"
+	appr_models "github.com/operator-framework/go-appr/models"
+)
+
+func TestRetrieveOne_PackageExists_SuccessExpected(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	adapter := NewMockapprApiAdapter(controller)
+	decoder := NewMockblobDecoder(controller)
+	unmarshaller := NewMockblobUnmarshaller(controller)
+
+	client := client{
+		adapter:      adapter,
+		decoder:      decoder,
+		unmarshaller: unmarshaller,
+	}
+
+	namespace := "redhat"
+	repository := "foo"
+	release := "1.0"
+	digest := "abcdefgh"
+
+	pkg := &appr_models.Package{Content: &appr_models.OciDescriptor{
+		Digest: digest,
+	}}
+	adapter.EXPECT().GetPackageMetadata(namespace, repository, release).Return(pkg, nil).Times(1)
+
+	blobExpected := []byte{'e', 'n', 'c', 'o', 'd', 'e', 'd'}
+	adapter.EXPECT().DownloadOperatorManifest(namespace, repository, digest).Return(blobExpected, nil).Times(1)
+
+	decodedExpected := []byte{'d', 'e', 'c', 'o', 'd', 'e', 'd'}
+	decoder.EXPECT().Decode(blobExpected).Return(decodedExpected, nil).Times(1)
+
+	manifestExpected := &Manifest{
+		Publisher: "redhat",
+		Data: Data{
+			CRDs:     "my crds",
+			CSVs:     "my csvs",
+			Packages: "my packages",
+		},
+	}
+	unmarshaller.EXPECT().Unmarshal(decodedExpected).Return(manifestExpected, nil)
+
+	metadata, err := client.RetrieveOne(fmt.Sprintf("%s/%s", namespace, repository), release)
+
+	assert.NoError(t, err)
+	assert.Equal(t, namespace, metadata.Namespace)
+	assert.Equal(t, repository, metadata.Repository)
+	assert.Equal(t, release, metadata.Release)
+	assert.Equal(t, digest, metadata.Digest)
+	assert.Equal(t, manifestExpected, metadata.Manifest)
+}

pkg/operatorsource/handler.go
@@ -1,0 +1,51 @@
+package operatorsource
+
+import (
+	"context"
+
+	"github.com/operator-framework/operator-marketplace/pkg/apis/marketplace/v1alpha1"
+	"github.com/operator-framework/operator-sdk/pkg/sdk"
+	"github.com/sirupsen/logrus"
+)
+
+// Handler is the interface that wraps the Hanlde method
+//
+// Handle hanldes a new event associated with OperatorSource type
+type Handler interface {
+	Handle(ctx context.Context, event sdk.Event) error
+}
+
+type handler struct {
+	reconciler Reconciler
+}
+
+func (h *handler) Handle(ctx context.Context, event sdk.Event) error {
+	opsrc := event.Object.(*v1alpha1.OperatorSource)
+
+	if event.Deleted {
+		logrus.Infof("No action taken, object has been deleted [type=%s object=%s/%s]",
+			opsrc.TypeMeta.Kind, opsrc.ObjectMeta.Namespace, opsrc.ObjectMeta.Name)
+
+		return nil
+	}
+
+	reconciled, err := h.reconciler.IsAlreadyReconciled(opsrc)
+	if err != nil {
+		return err
+	}
+
+	if reconciled {
+		logrus.Infof("Already reconciled, no action taken [type=%s object=%s/%s]",
+			opsrc.TypeMeta.Kind, opsrc.ObjectMeta.Namespace, opsrc.ObjectMeta.Name)
+
+		return nil
+	}
+
+	if err := h.reconciler.Reconcile(opsrc); err != nil {
+		return err
+	}
+
+	logrus.Infof("Reconciliation completed successfully [type=%s object=%s/%s]",
+		opsrc.TypeMeta.Kind, opsrc.ObjectMeta.Namespace, opsrc.ObjectMeta.Name)
+	return nil
+}