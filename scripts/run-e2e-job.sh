9cc76b8cc2ab9b3ce9f771c2570e853866aaa189
Aravindh Puthiyaparambil <aravindh@redhat.com>
2019-01-08 16:21:32 -0500

    [e2e] Stop creating Marketplace resources
    Now that we are managed by CVO, all the required Marketplace resources
    will be present in a cluster by default. This patch removes code that was
    creating these resources. Also removed downloading kubectl as that is
    not needed anymore.

--- scripts/run-e2e-job.sh
+++ scripts/run-e2e-job.sh
@@ -4,11 +4,10 @@ set -o pipefail
 
 MARKETPLACE_OPERATOR_ROOT=$(dirname "${BASH_SOURCE}")/..
 SDK_VERSION=v0.3.0
-KUBE_VERSION=v1.12.3
 
 # Get operator-sdk binary.
 wget -O /tmp/operator-sdk https://github.com/operator-framework/operator-sdk/releases/download/${SDK_VERSION}/operator-sdk-${SDK_VERSION}-x86_64-linux-gnu && chmod +x /tmp/operator-sdk
-wget -O /tmp/kubectl https://storage.googleapis.com/kubernetes-release/release/${KUBE_VERSION}/bin/linux/amd64/kubectl && chmod +x /tmp/kubectl
+
 PATH=$PATH:/tmp
 cd $MARKETPLACE_OPERATOR_ROOT
 . ./scripts/e2e-tests.sh
