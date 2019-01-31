38b53a79bb2bd7d5600c75489b721909c693ac2e
Aravindh Puthiyaparambil <aravindh@redhat.com>
2019-01-31 10:25:36 -0500

    [vendor] Update dependencies
    Update after adding github.com/openshift/api,
    github.com/openshift/client-go/ and github.com/openshift/library-go/

---
+++ vendor/github.com/openshift/library-go/pkg/operator/certrotation/client_cert_rotation_controller.go
@@ -0,0 +1,214 @@
+package certrotation
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang/glog"
+
+	operatorv1 "github.com/openshift/api/operator/v1"
+	"github.com/openshift/library-go/pkg/operator/v1helpers"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+)
+
+const (
+	// CertificateExpiryAnnotation contains the certificate expiration date in RFC3339 format.
+	CertificateExpiryAnnotation = "auth.openshift.io/certificate-expiry-date"
+	// CertificateSignedBy contains the common name of the certificate that signed another certificate.
+	CertificateSignedBy = "auth.openshift.io/certificate-signed-by"
+)
+
+const workQueueKey = "key"
+
+// CertRotationController does:
+//
+// 1) continuously create a self-signed signing CA (via SigningRotation).
+//    It creates the next one when a given percentage of the validity of the old CA has passed.
+// 2) maintain a CA bundle with all not yet expired CA certs.
+// 3) continuously create a target cert and key signed by the latest signing CA
+//    It creates the next one when a given percentage of the validity of the previous cert has
+//    passed, or when a new CA has been created.
+type CertRotationController struct {
+	name string
+
+	SigningRotation  SigningRotation
+	CABundleRotation CABundleRotation
+	TargetRotation   TargetRotation
+	OperatorClient   v1helpers.StaticPodOperatorClient
+
+	cachesSynced []cache.InformerSynced
+
+	// queue only ever has one item, but it has nice error handling backoff/retry semantics
+	queue workqueue.RateLimitingInterface
+}
+
+func NewCertRotationController(
+	name string,
+	signingRotation SigningRotation,
+	caBundleRotation CABundleRotation,
+	targetRotation TargetRotation,
+	operatorClient v1helpers.StaticPodOperatorClient,
+) (*CertRotationController, error) {
+	if !isOverlapSufficient(signingRotation, targetRotation) {
+		return nil, fmt.Errorf("insufficient overlap between signer and target")
+	}
+
+	ret := &CertRotationController{
+		SigningRotation:  signingRotation,
+		CABundleRotation: caBundleRotation,
+		TargetRotation:   targetRotation,
+		OperatorClient:   operatorClient,
+
+		cachesSynced: []cache.InformerSynced{
+			signingRotation.Informer.Informer().HasSynced,
+			caBundleRotation.Informer.Informer().HasSynced,
+			targetRotation.Informer.Informer().HasSynced,
+		},
+
+		queue: workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), name),
+	}
+
+	signingRotation.Informer.Informer().AddEventHandler(ret.eventHandler())
+	caBundleRotation.Informer.Informer().AddEventHandler(ret.eventHandler())
+	targetRotation.Informer.Informer().AddEventHandler(ret.eventHandler())
+
+	return ret, nil
+}
+
+func isOverlapSufficient(signingRotation SigningRotation, targetRotation TargetRotation) bool {
+	targetRefreshOverlap := float32(targetRotation.Validity) * (1 - targetRotation.RefreshPercentage)
+	requiredSignerAge := targetRefreshOverlap / 10
+	signerRefreshOverlap := float32(signingRotation.Validity) * (signingRotation.RefreshPercentage)
+	if signerRefreshOverlap < requiredSignerAge*2 {
+		return false
+	}
+	return true
+}
+
+func (c CertRotationController) sync() error {
+	syncErr := c.syncWorker()
+
+	condition := operatorv1.OperatorCondition{
+		Type:   "CertRotation_" + c.name + "_Failing",
+		Status: operatorv1.ConditionFalse,
+	}
+	if syncErr != nil {
+		condition.Status = operatorv1.ConditionTrue
+		condition.Reason = "RotationError"
+		condition.Message = syncErr.Error()
+	}
+	if _, _, updateErr := v1helpers.UpdateStaticPodStatus(c.OperatorClient, v1helpers.UpdateStaticPodConditionFn(condition)); updateErr != nil {
+		return updateErr
+	}
+
+	return syncErr
+}
+
+func (c CertRotationController) syncWorker() error {
+	signingCertKeyPair, err := c.SigningRotation.ensureSigningCertKeyPair()
+	if err != nil {
+		return err
+	}
+
+	cabundleCerts, err := c.CABundleRotation.ensureConfigMapCABundle(signingCertKeyPair)
+	if err != nil {
+		return err
+	}
+
+	if err := c.TargetRotation.ensureTargetCertKeyPair(signingCertKeyPair, cabundleCerts); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func needNewCertKeyPairForTime(annotations map[string]string, validity time.Duration, renewalPercentage float32) bool {
+	signingCertKeyPairExpiry := annotations[CertificateExpiryAnnotation]
+	if len(signingCertKeyPairExpiry) == 0 {
+		return true
+	}
+	certExpiry, err := time.Parse(time.RFC3339, signingCertKeyPairExpiry)
+	if err != nil {
+		glog.Infof("bad expiry: %q", signingCertKeyPairExpiry)
+		// just create a new one
+		return true
+	}
+
+	// If Certificate is not-expired, skip this iteration.
+	renewalDuration := -1 * float32(validity) * (1 - renewalPercentage)
+	if certExpiry.Add(time.Duration(renewalDuration)).After(time.Now()) {
+		return false
+	}
+
+	return true
+}
+
+func (c *CertRotationController) Run(workers int, stopCh <-chan struct{}) {
+	defer utilruntime.HandleCrash()
+	defer c.queue.ShutDown()
+
+	glog.Infof("Starting CertRotationController - %q", c.name)
+	defer glog.Infof("Shutting down CertRotationController - %q", c.name)
+
+	if !cache.WaitForCacheSync(stopCh, c.cachesSynced...) {
+		utilruntime.HandleError(fmt.Errorf("caches did not sync"))
+		return
+	}
+
+	// doesn't matter what workers say, only start one.
+	go wait.Until(c.runWorker, time.Second, stopCh)
+
+	// start a time based thread to ensure we stay up to date
+	go wait.Until(func() {
+		ticker := time.NewTicker(time.Minute)
+		defer ticker.Stop()
+
+		for {
+			c.queue.Add(workQueueKey)
+			select {
+			case <-ticker.C:
+			case <-stopCh:
+				return
+			}
+		}
+
+	}, time.Minute, stopCh)
+
+	<-stopCh
+}
+
+func (c *CertRotationController) runWorker() {
+	for c.processNextWorkItem() {
+	}
+}
+
+func (c *CertRotationController) processNextWorkItem() bool {
+	dsKey, quit := c.queue.Get()
+	if quit {
+		return false
+	}
+	defer c.queue.Done(dsKey)
+
+	err := c.sync()
+	if err == nil {
+		c.queue.Forget(dsKey)
+		return true
+	}
+
+	utilruntime.HandleError(fmt.Errorf("%v failed with : %v", dsKey, err))
+	c.queue.AddRateLimited(dsKey)
+
+	return true
+}
+
+// eventHandler queues the operator to check spec and status
+func (c *CertRotationController) eventHandler() cache.ResourceEventHandler {
+	return cache.ResourceEventHandlerFuncs{
+		AddFunc:    func(obj interface{}) { c.queue.Add(workQueueKey) },
+		UpdateFunc: func(old, new interface{}) { c.queue.Add(workQueueKey) },
+		DeleteFunc: func(obj interface{}) { c.queue.Add(workQueueKey) },
+	}
+}
---
+++ vendor/github.com/openshift/library-go/pkg/operator/certrotation/signer.go
@@ -0,0 +1,100 @@
+package certrotation
+
+import (
+	"bytes"
+	"fmt"
+	"time"
+
+	"github.com/openshift/library-go/pkg/crypto"
+	"github.com/openshift/library-go/pkg/operator/events"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1informers "k8s.io/client-go/informers/core/v1"
+	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
+	corev1listers "k8s.io/client-go/listers/core/v1"
+)
+
+// SigningRotation rotates a self-signed signing CA stored in a secret. It creates a new one when <RefreshPercentage>
+// of the lifetime of the old CA has passed.
+type SigningRotation struct {
+	Namespace         string
+	Name              string
+	Validity          time.Duration
+	RefreshPercentage float32
+
+	Informer      corev1informers.SecretInformer
+	Lister        corev1listers.SecretLister
+	Client        corev1client.SecretsGetter
+	EventRecorder events.Recorder
+}
+
+func (c SigningRotation) ensureSigningCertKeyPair() (*crypto.CA, error) {
+	originalSigningCertKeyPairSecret, err := c.Lister.Secrets(c.Namespace).Get(c.Name)
+	if err != nil && !apierrors.IsNotFound(err) {
+		return nil, err
+	}
+	signingCertKeyPairSecret := originalSigningCertKeyPairSecret.DeepCopy()
+	if apierrors.IsNotFound(err) {
+		// create an empty one
+		signingCertKeyPairSecret = &corev1.Secret{ObjectMeta: metav1.ObjectMeta{Namespace: c.Namespace, Name: c.Name}}
+	}
+	signingCertKeyPairSecret.Type = corev1.SecretTypeTLS
+
+	if needNewSigningCertKeyPair(signingCertKeyPairSecret.Annotations, c.Validity, c.RefreshPercentage) {
+		c.EventRecorder.Eventf("SignerUpdateRequired", "%q in %q requires a new signing cert/key pair", c.Name, c.Namespace)
+		if err := setSigningCertKeyPairSecret(signingCertKeyPairSecret, c.Validity); err != nil {
+			return nil, err
+		}
+
+		actualSigningCertKeyPairSecret, err := c.Client.Secrets(c.Namespace).Update(signingCertKeyPairSecret)
+		if apierrors.IsNotFound(err) {
+			actualSigningCertKeyPairSecret, err = c.Client.Secrets(c.Namespace).Create(signingCertKeyPairSecret)
+			if err != nil {
+				return nil, err
+			}
+		}
+		if err != nil {
+			return nil, err
+		}
+		signingCertKeyPairSecret = actualSigningCertKeyPairSecret
+	}
+	// at this point, the secret has the correct signer, so we should read that signer to be able to sign
+	signingCertKeyPair, err := crypto.GetCAFromBytes(signingCertKeyPairSecret.Data["tls.crt"], signingCertKeyPairSecret.Data["tls.key"])
+	if err != nil {
+		return nil, err
+	}
+
+	return signingCertKeyPair, nil
+}
+
+func needNewSigningCertKeyPair(annotations map[string]string, validity time.Duration, renewalPercentage float32) bool {
+	return needNewCertKeyPairForTime(annotations, validity, renewalPercentage)
+}
+
+// setSigningCertKeyPairSecret creates a new signing cert/key pair and sets them in the secret
+func setSigningCertKeyPairSecret(signingCertKeyPairSecret *corev1.Secret, validity time.Duration) error {
+	signerName := fmt.Sprintf("%s_%s@%d", signingCertKeyPairSecret.Namespace, signingCertKeyPairSecret.Name, time.Now().Unix())
+	ca, err := crypto.MakeCAConfigForDuration(signerName, validity)
+	if err != nil {
+		return err
+	}
+
+	certBytes := &bytes.Buffer{}
+	keyBytes := &bytes.Buffer{}
+	if err := ca.WriteCertConfig(certBytes, keyBytes); err != nil {
+		return err
+	}
+
+	if signingCertKeyPairSecret.Annotations == nil {
+		signingCertKeyPairSecret.Annotations = map[string]string{}
+	}
+	if signingCertKeyPairSecret.Data == nil {
+		signingCertKeyPairSecret.Data = map[string][]byte{}
+	}
+	signingCertKeyPairSecret.Data["tls.crt"] = certBytes.Bytes()
+	signingCertKeyPairSecret.Data["tls.key"] = keyBytes.Bytes()
+	signingCertKeyPairSecret.Annotations[CertificateExpiryAnnotation] = ca.Certs[0].NotAfter.Format(time.RFC3339)
+
+	return nil
+}
