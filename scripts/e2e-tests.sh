e6a94754438cd16c4524bf48206695932ced84f5
kevinrizza <krizza@redhat.com>
2019-08-26 10:53:50 -0400

    Go test timeout fix
    e2e tests fail now with the addition of the operator restart test
    which checks that default opsrces are recreated (due to the length
    of that test). Increasing the default e2e timeout to 20 minutes to
    allow the tests to complete.
    
    In the future, these tests should be enhanced. Many of them could
    run in parallel with proper consideration.

--- scripts/e2e-tests.sh
+++ scripts/e2e-tests.sh
@@ -10,4 +10,4 @@ fi
 
 # Run the tests through the operator-sdk
 echo "Running operator-sdk test"
-operator-sdk test local ./test/e2e/ --no-setup --go-test-flags -v --namespace $TEST_NAMESPACE
+operator-sdk test local ./test/e2e/ --no-setup --go-test-flags "-v -timeout 20m" --namespace $TEST_NAMESPACE
