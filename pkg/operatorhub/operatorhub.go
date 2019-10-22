447dd7b3a9d23c8fa120d797db31e3b479d19855
kevinrizza <krizza@redhat.com>
2019-10-22 10:57:25 -0400

    Assume available status is true without defaults
    Problem:
    The clusteroperator operator status is never set to Available=True when
    there are no default operator sources set. This is because it relies on
    a set of reconciliations to return successfully and determines a ratio
    based on that value. When no default operator sources are enabled, there
    are no reconciliation events taking place and therefore the ratio is
    always set to zero.
    
    Solution:
    When no default operator sources are enabled and no events are firing,
    set the Available status to true.
    
    Additionally, reduce the number of minimum events before reporting
    status in the event that only a single default operatorsource is enabled

--- pkg/operatorhub/operatorhub.go
+++ pkg/operatorhub/operatorhub.go
@@ -32,6 +32,7 @@ type operatorhub struct {
 type OperatorHub interface {
 	Get() map[string]bool
 	Set(spec configv1.OperatorHubSpec)
+	Disabled() bool
 }
 
 // GetSingleton returns the singleton instance of HubConfig
@@ -44,6 +45,21 @@ func (o *operatorhub) Get() map[string]bool {
 	o.lock.Lock()
 	defer o.lock.Unlock()
 	return o.current
+}
+
+// Disabled returns true if all defaults are disabled
+func (o *operatorhub) Disabled() bool {
+	o.lock.Lock()
+	defer o.lock.Unlock()
+
+	disabled := true
+	for _, disabled := range o.current {
+		if disabled == false {
+			disabled = false
+		}
+	}
+
+	return disabled
 }
 
 // Set sets the current configuration based on the spec. If the spec is empty,
