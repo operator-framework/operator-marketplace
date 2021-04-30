6af4a319eb294a46039285753013d17839843750
timflannagan <timflannagan@gmail.com>
2021-04-30 11:32:36 -0400

    scripts: Update the e2e-related scripts
    - Verify that the $KUBECONFIG variable has been set.
    - Remove the GO111MODULE=off declaration now that we've migrated from
      dep to Go modules.
    - Remove the need to check for script parameters for the $TEST_NAMESPACE
      environment variable by defaulting to "openshift-marketplace", but
      respecting any overrides from the call site.
    - Introduce an $EXTRA_GO_TEST_FLAGS environment variable that can be
      used to pass Go test flag(s), like "-run <specific test>", to the e2e
      suite when running developing and debugging locally.
    - Updates to how the operator-sdk test command(s) work now after bumping
      the binary to v0.19.x.
    - Support overriding the OPERATOR_SDK_BIN environment variable in the
      run-e2e-jobs.sh bash script.

--- scripts/e2e-tests.sh
+++ scripts/e2e-tests.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+
 set -o errexit
 set -o nounset
 set -o pipefail
 
-ARG1=${1-}
-if [ "$ARG1" = "minikube" ]; then
-    TEST_NAMESPACE="marketplace"
-else
-    TEST_NAMESPACE="openshift-marketplace"
-fi
+: "${KUBECONFIG:?}"
 
+TEST_NAMESPACE="${TEST_NAMESPACE:=openshift-marketplace}"
 OPERATOR_SDK_BIN=${OPERATOR_SDK_BIN:=operator-sdk}
-export GO111MODULE=off
+EXTRA_GO_TEST_FLAGS=${EXTRA_GO_TEST_FLAGS:=""}
 
-# Run the tests through the operator-sdk
-echo "Running operator-sdk test"
-${OPERATOR_SDK_BIN} --version
-${OPERATOR_SDK_BIN} test local ./test/e2e/ --no-setup --go-test-flags "-v -timeout 50m" --namespace $TEST_NAMESPACE
+echo "Running the e2e testing suite"
+${OPERATOR_SDK_BIN} version
+${OPERATOR_SDK_BIN} test local \
+    ./test/e2e/ \
+    --debug \
+    --no-setup \
+    --kubeconfig "${KUBECONFIG}" \
+    --operator-namespace ${TEST_NAMESPACE} \
+    --go-test-flags "-v -timeout 50m ${EXTRA_GO_TEST_FLAGS}"
