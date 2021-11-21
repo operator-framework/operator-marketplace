7835f054247b7f94bb471f88cb1ff2726bd4feb9
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-21 22:20:33 +0800

    Delete pkg directory

--- pkg/controller/add_catalogsource.go
+++
@@ -1,8 +0,0 @@
-package controller
-
-import "github.com/operator-framework/operator-marketplace/pkg/controller/catalogsource"
-
-func init() {
-	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
-	AddToManagerFuncs = append(AddToManagerFuncs, catalogsource.Add)
-}
--- pkg/controller/options/options.go
+++
@@ -1,4 +0,0 @@
-package options
-
-type ControllerOptions struct {
-}
